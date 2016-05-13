381a88f514470f9294ef618839178bff6e35af34
abhishek <abhishek@abhisheks-mbp.mshome.net>
2016-05-13 11:00:08 -0500

    Signed-off-by: abhishek <abhishek@abhisheks-mbp.mshome.net>

---
+++ src/main/java/com/hrm/domain/Employee.java
@@ -0,0 +1,20 @@
+package com.hrm.domain;
+
+import java.util.Date;
+
+public class Employee {
+	
+	private long employeeId;
+	private String firstName;
+	private String lastName;
+	private Date dob;
+	private String sex;
+	private String maritalStatus;
+	private String email;
+	private String contactNumber;
+	private Address address;
+	private Date joinDate;
+	private Date endDate;
+	private boolean Status;
+
+}
