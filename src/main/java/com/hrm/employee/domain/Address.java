ef78863958b018d5e528dcbf3b0769dbd2bd0572
abhishek <abhishek@abhisheks-mbp.mshome.net>
2016-05-13 13:36:40 -0500

    Signed-off-by: abhishek <abhishek@abhisheks-mbp.mshome.net>

--- src/main/java/com/hrm/employee/domain/Address.java
+++ src/main/java/com/hrm/employee/domain/Address.java
@@ -1,11 +1,23 @@
-package com.hrm.domain;
+package com.hrm.employee.domain;
 
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class Address {
 	private String street;
 	private String city;
 	private String state;
 	private int zip;
 	private String country;
+
+	public Address(String street, String city, String state, int zip, String country) {
+		
+		this.street = street;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.country = country;
+	}
 
 	public String getStreet() {
 		return street;
